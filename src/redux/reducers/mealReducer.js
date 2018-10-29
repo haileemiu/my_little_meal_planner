6e04d14d14aac075e47bebc521a7c1356544ab91
Hailee Miu <hailee.miu@gmail.com>
2018-10-29 08:52:44 -0500

    close calendar-inputs

--- src/redux/reducers/mealReducer.js
+++ src/redux/reducers/mealReducer.js
@@ -9,7 +9,7 @@ const mealReducer = (state = initialState, action) => {
       // console.log('action.payload in mealReducer:', action.payload.data.recipes);
       return { ...state, recipes: action.payload.data.recipes };
     case 'GET_MEALS_RESPONSE':
-      console.log('action.payload in get meals response meal reducer:', action.payload.data);
+      // console.log('action.payload in get meals response meal reducer:', action.payload.data);
       return { ...state, meals: action.payload };
     case 'REMOVE_MEAL_RESPONSE':
       return { ...state, meals: state.meals.filter(meal => meal.id !== action.payload) }
